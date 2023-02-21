2bf4df03612027331a8808f76eaf2edb4fcaf61c
Akin59452 <aki59452@gmail.com>
2023-02-21 14:53:35 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include &quot;main.h&quot;
+
+/**
+
+* main - print _putchar
+
+* is written by mbah
+
+* Return: 0
+
+*/
+
+int main(void)
+
+{
+
+_putchar(&#39;_&#39;);
+
+_putchar(&#39;p&#39;);
+
+_putchar(&#39;u&#39;);
+
+_putchar(&#39;t&#39;);
+
+_putchar(&#39;c&#39;);
+
+_putchar(&#39;h&#39;);
+
+_putchar(&#39;a&#39;);
+
+_putchar(&#39;r&#39;);
+
+_putchar(&#39;\n&#39;);
+
+return (0);
+
+}
